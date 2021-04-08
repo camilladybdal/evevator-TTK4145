e2d749ea3254c1763149452504252c1b91acff7c
CamzillaD <camildy@stud.ntnu.no>
2021-04-08 21:22:37 +0200

    test for new timer function

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	//"./elevio"
+	//"./fsm"
+	"./timer"
+)
+
 func main() {
+
+	TimedOut := make(chan bool)
+	go timer.DoorTimer(3, TimedOut)
+
+	if <-TimedOut {
+		println("finished timer")
+	}
 
 }
