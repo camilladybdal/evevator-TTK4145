ac83c7fdd987c6e28e4422af7b1f9c4f4cab5306
CamzillaD <camildy@stud.ntnu.no>
2021-04-09 16:17:29 +0200

    moved structs and const values to new file

---
+++ types/types.go
@@ -0,0 +1,39 @@
+package types
+
+const (
+	NumFloors    int = 4
+	DOOROPENTIME int = 3
+)
+
+type State int
+
+const (
+	IDLE     State = 0
+	MOVING         = 1
+	DOOROPEN       = 2
+)
+
+type Elevator struct {
+	UpQueue      [NumFloors]int
+	DownQueue    [NumFloors]int
+	CurrentFloor int
+	Direction    int
+}
+
+// Constants
+const (
+	NumberOfElevators = 3 // Need better implemantation (config fil?)
+	NumberOfFloors    = 4 // also config?
+	maxCost           = 999999999
+	elevatorId        = 0
+)
+
+// Structures
+type Order struct {
+	Floor         int
+	DirectionUp   bool
+	DirectionDown bool
+	Cost          [NumberOfElevators]int
+	Status        int  // 0: No active order , 1: waiting for cost, 2: unconfirmed, 3: confirmed, 4: mine, 5: done
+	TimedOut      bool // Time? or Id?
+}
