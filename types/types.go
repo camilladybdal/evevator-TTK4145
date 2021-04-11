bafe14fe04440ab0a92987f9dfdc87631248ceac
CamzillaD <camildy@stud.ntnu.no>
2021-04-11 20:36:57 +0200

    moved FsmChannels to types

--- types/types.go
+++ types/types.go
@@ -11,14 +11,16 @@ const (
 	IDLE     State = 0
 	MOVING         = 1
 	DOOROPEN       = 2
+	ERROR          = 3
 )
 
-type Elevator struct {
-	UpQueue      [NumFloors]int
-	DownQueue    [NumFloors]int
-	CurrentFloor int
-	Direction    int
-}
+type Direction int
+const (
+	Up 	  Direction = 1
+	Down 			= -1
+	Stop			= 0
+)
+
 
 // Constants
 const (
@@ -28,6 +30,7 @@ const (
 	elevatorId        = 0
 )
 
+
 // Structures
 type Order struct {
 	Floor         int
@@ -36,4 +39,21 @@ type Order struct {
 	Cost          [NumberOfElevators]int
 	Status        int  // 0: No active order , 1: waiting for cost, 2: unconfirmed, 3: confirmed, 4: mine, 5: done
 	TimedOut      bool // Time? or Id?
+	id            int
+}
+
+type FsmChannels struct {
+	FloorReached   chan int
+	MotorDirection chan int
+	NewOrder       chan Order
+	Obstruction    chan bool
+	Stop           chan bool
+	ElevatorState  chan Elevator
+}
+
+type Elevator struct {
+	UpQueue      [NumFloors]int
+	DownQueue    [NumFloors]int
+	CurrentFloor int
+	Direction    int
 }
