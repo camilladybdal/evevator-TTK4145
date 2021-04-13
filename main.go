1a9b715d3f0eccf0e3a858e55fc09f61e0e5ad72
aashild <aashilbr@stud.ntnu.no>
2021-04-13 10:33:39 +0200

    main ditch

--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
-	//"./elevio"
-	//"./fsm"
-	"./timer"
+	"./costfnc"
+	. "./types"
 )
 
 func main() {
-
-	TimedOut := make(chan bool)
-	go timer.DoorTimer(3, TimedOut)
-
-	if <-TimedOut {
-		println("finished timer")
+	
+	var neworder Order
+	neworder.Floor = 1
+	neworder.DirectionUp = true
+	var elevator Elevator
+	elevator.CurrentFloor = 2
+	elevator.Direction = 0
+	for i := 0; i < NumFloors; i++ {
+		elevator.UpQueue[i] = 0
 	}
-
+	elevator.UpQueue[0] = 1
+	//elevator.UpQueue[2] = 1
+	for i := 0; i < NumFloors; i++ {
+		elevator.DownQueue[i] = 0
+	}
+	elevator.DownQueue[3] = 1
+	cost := costfnc.Costfunction(elevator, neworder)
+	println(cost)
 }
