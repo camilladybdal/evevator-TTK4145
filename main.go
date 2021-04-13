b1425ebaff20593b0401dd82c60152a311c35785
dot3dash <jonska@stud.ntnu.no>
2021-04-13 15:01:17 +0200

    broadcast hello worked, testing order
    Signed-off-by: dot3dash <jonska@stud.ntnu.no>

--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"fmt"
-
-	"./config"
 
 	//"./fsm"
 	//"./timer"
-	"time"
-
-	"./network/bcast"
-	//. "./orderDistributor"
-	//. "./types"
+	"./elevio"
+	. "./orderDistributor"
+	. "./types"
 )
 
 type HelloMsg struct {
@@ -20,39 +15,40 @@ type HelloMsg struct {
 }
 
 func main() {
-	/*
-		orderOut := make(chan Order)
-		orderIn := make(chan Order)
-		getElevatorState := make(chan Elevator)
 
-		elevio.Init("localhost:15657", 4)
+	orderOut := make(chan Order)
+	orderIn := make(chan Order)
+	getElevatorState := make(chan Elevator)
 
-		go OrderDistributor(orderOut, orderIn, getElevatorState)
-	*/
+	elevio.Init("localhost:15657", 4)
+
+	go OrderDistributor(orderOut, orderIn, getElevatorState)
+	for {
+	}
 	//networkTransmit := make(chan Order)
 	//networkReceive := make(chan Order)
+	/*
+		helloTx := make(chan HelloMsg)
+		helloRx := make(chan HelloMsg)
 
-	helloTx := make(chan HelloMsg)
-	helloRx := make(chan HelloMsg)
+		go bcast.Transmitter(config.Port, helloTx)
+		go bcast.Receiver(config.Port, helloRx)
 
-	go bcast.Transmitter(config.Port, helloTx)
-	go bcast.Receiver(config.Port, helloRx)
+		go func() {
+			helloMsg := HelloMsg{"Helloooo", 0}
+			for {
+				helloMsg.Iter++
+				helloTx <- helloMsg
+				time.Sleep(1 * time.Second)
+			}
+		}()
 
-	go func() {
-		helloMsg := HelloMsg{"Helloooo", 0}
 		for {
-			helloMsg.Iter++
-			helloTx <- helloMsg
-			time.Sleep(1 * time.Second)
-		}
-	}()
-
-	for {
-		select {
-		case a := <-helloRx:
-			fmt.Println("Received: %#v\n", a)
-		}
-	}
+			select {
+			case a := <-helloRx:
+				fmt.Println("Received: %#v\n", a)
+			}
+		}*/
 	/*
 		elevator.UpQueue[0] = 1
 		//elevator.UpQueue[2] = 1
