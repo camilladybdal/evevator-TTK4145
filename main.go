b375a151da6b8eddece0facc662f2bf35e08d14b
dot3dash <jonska@stud.ntnu.no>
2021-04-17 10:38:45 +0200

    test for resetable timer
    Signed-off-by: dot3dash <jonska@stud.ntnu.no>

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 
-	. "./FSM"
-	//"./timer"
-	"./elevio"
-	. "./orderDistributor"
-	. "./types"
+	//. "./FSM"
+	"./timer"
+	//"./elevio"
+	//. "./orderDistributor"
+	//. "./types"
 	"fmt"
-	."./config"
+	//."./config"
+	"time"
 )
 
 type HelloMsg struct {
@@ -17,7 +18,38 @@ type HelloMsg struct {
 }
 
 func main() {
+	test := 10000
+	test2 := 100000
+	var duration time.Duration
+	duration = 1
+	newCountdownTime := make(chan time.Duration)
+	timedOut := make(chan bool)
 
+	go timer.ResetableTimer(duration, newCountdownTime, timedOut)
+
+	for {
+		select {
+		case <- timedOut:
+			fmt.Println("Timed out")
+			//newCountdownTime <- 1
+		default:
+			if test > 0 {
+				newCountdownTime <- 1
+				test--
+				test2 = 1000000000
+			} else {
+				//newCountdownTime <- duration
+				//fmt.Println("!")
+				if test2 < 0 {
+					fmt.Println("?")
+					test = 100000
+				} 
+				test2--
+			}
+		}
+	}
+
+	/*
 	fmt.Println("LETS GO")
 	
 	// FSM channels
@@ -41,7 +73,7 @@ func main() {
 	go OrderDistributor(fsmChannels.NewOrder, orderUpdate, getElevatorState)
 	go RunElevator(fsmChannels, orderUpdate, getElevatorState)
 
-
+	*/
 	for {}
 
 }
