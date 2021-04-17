ffb59c55fe68940b80ecd799a7b9c770cd7f536b
aashild <aashilbr@stud.ntnu.no>
2021-04-17 10:25:26 +0200

    Added some file functions

--- FSM/fsmUtilities.go
+++ FSM/fsmUtilities.go
@@ -1,10 +1,15 @@
 package fsm
 
 import (
+	"fmt"
+	"os"
+
+	"bufio"
+	"strconv"
+
+	. "../config.go"
 	"../elevio"
 	. "../types"
-	. "../config.go"
-	"fmt"
 )
 
 func getDirection(currentFloor int, destinationFloor int) elevio.MotorDirection {
@@ -15,7 +20,7 @@ func getDirection(currentFloor int, destinationFloor int) elevio.MotorDirection 
 	}
 }
 
-func checkOrdersPresent(elevator Elevator) bool{
+func checkOrdersPresent(elevator Elevator) bool {
 	foundOrder := false
 	for i := 0; i < NumberOfFloors; i++ {
 		if elevator.UpQueue[i] == 1 || elevator.DownQueue[i] == 1 {
@@ -29,25 +34,24 @@ func queueSearch(QueueDirection elevio.MotorDirection, elevator Elevator) int {
 	nextFloor := -1
 
 	//first times
-	if QueueDirection == elevio.MD_Stop{
+	if QueueDirection == elevio.MD_Stop {
 		QueueDirection = elevio.MD_Up
 	}
-	
 
-	fmt.Println(" ---- QUEUESEARCH, MY DIRECTION IS: ", QueueDirection);
-	fmt.Println(" ---- QUEUESEARCH, MY CUREENT FLOOR IS: ", elevator.CurrentFloor);
+	fmt.Println(" ---- QUEUESEARCH, MY DIRECTION IS: ", QueueDirection)
+	fmt.Println(" ---- QUEUESEARCH, MY CUREENT FLOOR IS: ", elevator.CurrentFloor)
 
 	/*
-	fmt.Println("Upqueue:: ")
-	for i:=0;i<NumberOfFloors;i++{
-		fmt.Println(elevator.UpQueue[i])
-	}
-	fmt.Println("Downqueue: ")
-	for i:=0;i<NumberOfFloors;i++{
-	fmt.Println(elevator.DownQueue[i])
-	}
-    */
-	
+		fmt.Println("Upqueue:: ")
+		for i:=0;i<NumberOfFloors;i++{
+			fmt.Println(elevator.UpQueue[i])
+		}
+		fmt.Println("Downqueue: ")
+		for i:=0;i<NumberOfFloors;i++{
+		fmt.Println(elevator.DownQueue[i])
+		}
+	*/
+
 	if QueueDirection == elevio.MD_Up {
 		for floor := elevator.CurrentFloor; floor < NumberOfFloors; floor++ {
 			if elevator.UpQueue[floor] == 1 {
@@ -70,7 +74,7 @@ func queueSearch(QueueDirection elevio.MotorDirection, elevator Elevator) int {
 		}
 	}
 
-	if QueueDirection == elevio.MD_Down{
+	if QueueDirection == elevio.MD_Down {
 
 		for floor := elevator.CurrentFloor; floor >= 0; floor-- {
 			if elevator.DownQueue[floor] == 1 {
@@ -84,7 +88,7 @@ func queueSearch(QueueDirection elevio.MotorDirection, elevator Elevator) int {
 				return nextFloor
 			}
 		}
-		for floor := NumberOfFloors-1; floor >= 0; floor-- {
+		for floor := NumberOfFloors - 1; floor >= 0; floor-- {
 			if elevator.DownQueue[floor] == 1 {
 				nextFloor = floor
 				return nextFloor
@@ -94,14 +98,54 @@ func queueSearch(QueueDirection elevio.MotorDirection, elevator Elevator) int {
 	return nextFloor
 }
 
-func removeFromQueue(elevator *Elevator){
-		elevator.UpQueue[elevator.CurrentFloor] = 0
-		elevator.DownQueue[elevator.CurrentFloor] = 0
+func removeFromQueue(elevator *Elevator) {
+	elevator.UpQueue[elevator.CurrentFloor] = 0
+	elevator.DownQueue[elevator.CurrentFloor] = 0
 }
 
-func emptyQueue(elevator *Elevator){
+func emptyQueue(elevator *Elevator) {
 	for floor := 0; floor < NumberOfFloors; floor++ {
 		elevator.UpQueue[floor] = 0
 		elevator.DownQueue[floor] = 0
 	}
+}
+
+////////////FILE/////////////
+func checkError(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func openBackupFile() {
+	if _, err := os.Stat("backupCabOrders.txt"); os.IsNotExist(err) {
+		filename, err := os.Create("backupCabOrders.txt")
+		checkError(err)
+		defer filename.Close()
+		for i := 0; i < NumberOfFloors; i++ {
+			_, err1 := filename.WriteString(fmt.Sprintf("%d\n", 0))
+			checkError(err1)
+		}
+	}
+	filename, err := os.Open("backupCabOrders.txt")
+	checkError(err)
+	defer filename.Close()
+}
+
+func readFromBackupFile(filename *os.File) []int {
+	var CabOrderArray []int
+	scanner := bufio.NewScanner(filename)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		data, err := strconv.Atoi(scanner.Text())
+		checkError(err)
+		CabOrderArray = append(CabOrderArray, data)
+	}
+	return CabOrderArray
+}
+
+func writeToBackUpFile(filename *os.File) {
+	//når du får orderen inn i fsm, så må du skrive ned til fil
+	// dvs. skriv 1 til fil på den linja du har floor til
+	//når du har utført orderen må du skrive 0 til linja med floor-nr
 }
